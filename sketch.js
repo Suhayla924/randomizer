cb16bc8f021701c5df399209e14c3293b2d14a00
Suhayla924 <suhaylabhassan@gmail.com>
2020-09-27 19:13:54 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
+// dogs i have loved before
+let dogs = ["dillon", "Ziggy", "mercedes", "nala", "kloot", "petunia"];
 function setup() {
   createCanvas(600, 600);
   background(220);
+
+dogs[1] ="ziggy stardust"
+  console.log(dogs);
+
 }
 
 function draw() {
-  
+
 }
