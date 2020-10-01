eca8f8a42df9a0f4ef048c654bcbbfcd59fbc654
Suhayla924 <suhaylabhassan@gmail.com>
2020-10-01 15:22:33 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,76 +1,110 @@
 // Parks that I have visted with my family <3
-let parks= [{
-  name:"yellowstone", color:"yellow"
+let parks = [{
+  name: "yellowstone",
+  color: "yellow"
 }, {
-  name:"grand canyon", color:"rust"
-},{
-  name:"sequioa", color: "forest"
+  name: "grand canyon",
+  color: "rust"
 }, {
-  name:"death", color:"sandy"
+  name: "sequioa",
+  color: "forest"
 }, {
-  name:"bryce", color:"orange"
-},{
-  name:"arches", color:"mars"
-},{
-  name:"zion", color:"valleygreen"
-},{
-  name:"Yosemite", color:"snowy"
+  name: "death",
+  color: "sandy"
+}, {
+  name: "bryce",
+  color: "orange"
+}, {
+  name: "arches",
+  color: "mars"
+}, {
+  name: "zion",
+  color: "valleygreen"
+}, {
+  name: "Yosemite",
+  color: "snowy"
 }];
 
 let randomIndex;
+let animating = false;
+
 
 function setup() {
   createCanvas(600, 600);
-  background(220);
+  background(150);
+  textSize(35);
+
+  text("click to randomize", 50, 50);
+
 
 }
 
 function draw() {
 
+  if (animating == true) {
+    ellipse(random(width), random(height), random(25, 200))
+
+  }
 }
 
-function mousePressed(){
-  background(random(200,255));
-  randomIndex = int(random(parks.length));
-    text(parks[randomIndex].name,50,50);
+function randomizer() {
+  animation = false;
+  if (parks[0]) {
+    //displays random name and splices it out of the array list
+    background(random(80, 255));
+    randomIndex = int(random(parks.length));
+    text(`${parks[randomIndex].name}'s favorite color is
+    ${parks[randomIndex].color}`, 100, 100);
+    //text(parks[randomIndex].name + "'s favorite color is" +
+    //parks[randomIndex].color, 50, 50);
     parks.splice(randomIndex, 1);
-
+  } else {
+    background(random(80, 255));
+    text("nothing left!", 50, 50);
+  }
 }
 
-      //notes
-                //(object.properties)
-      //console.log(parks[0].name);
+function mousePressed() {
+  animating = true;
+  setTimeout(randomizer, 2000);
+}
 
-      //*computers start counting at zero
-      //<--------Looking up the values of an array--------->
-      //parks[1] ="Grand Canyon National Park"
-      //parks[3]="Death Valley National Park"
-      //parks[4]="Bryce Canyon National Park"
 
-      //console.log("intitial array is")
-      //console.log(parks);
 
-      //<--taking off the last item-->
-      //parks.pop();
-      //console.log("array after pop")
-      //console.log(parks);
+//notes
+//(object.properties)
+//console.log(parks[0].name);
 
-      //<--adding an item at the end of the list-->
-      //  parks.push("shasta");
-      //console.log("array after push")
-      //console.log(parks);
+//*computers start counting at zero
+//<--------Looking up the values of an array--------->
+//parks[1] ="Grand Canyon National Park"
+//parks[3]="Death Valley National Park"
+//parks[4]="Bryce Canyon National Park"
 
-      //<--taking out items from the beginning-->
-      //parks.shift();
-      //console.log("array after pop")
-      //console.log(parks);
+//console.log("intitial array is")
+//console.log(parks);
 
-      //<--adds items to the beginning-->
-      //parks.unshift("shasta");
-      //console.log("array after push")
-      //console.log(parks);
+//<--taking off the last item-->
+//parks.pop();
+//console.log("array after pop")
+//console.log(parks);
 
-      //<--removing items from the middle of an array (index,number)-->
-      //parks.splice(4, 1);
-      //console.log("array after push")
-      //console.log(parks);
+//<--adding an item at the end of the list-->
+//  parks.push("shasta");
+//console.log("array after push")
+//console.log(parks);
+
+//<--taking out items from the beginning-->
+//parks.shift();
+//console.log("array after pop")
+//console.log(parks);
+
+//<--adds items to the beginning-->
+//parks.unshift("shasta");
+//console.log("array after push")
+//console.log(parks);
+
+//<--removing items from the middle of an array (index,number)-->
+//parks.splice(4, 1);
+//console.log("array after push")
+//console.log(parks);
