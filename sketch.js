8766bab06c0753c4124a8ff402ec0307cc218ca3
Suhayla924 <suhaylabhassan@gmail.com>
2020-09-30 18:35:01 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
-// dogs i have loved before
-let dogs = ["dillon", "Ziggy", "mercedes", "nala", "kloot", "petunia"];
+// Parks that I have visted with my family <3
+let parks = ["yellowstone","grand", "sequioa", "death", "bryce", "arches", "zion", "Yosemite"];
+
 function setup() {
   createCanvas(600, 600);
   background(220);
 
-dogs[1] ="ziggy stardust"
-  console.log(dogs);
+  //*computers start counting at zero
+  //<--------Looking up the values of an array--------->
+  //parks[1] ="Grand Canyon National Park"
+  //parks[3]="Death Valley National Park"
+  //parks[4]="Bryce Canyon National Park"
+
+  console.log("intitial array is")
+  console.log(parks);
+
+  parks.pop();
+  console.log("array after pop")
+  console.log(parks);
+
+  dogs.push("shasta");
+  console.log("array after push")
+  console.log(parks);
 
 }
 
