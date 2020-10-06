beef49d90bbf466da0c2a4c9aa8b403541bd9395
Suhayla924 <suhaylabhassan@gmail.com>
2020-10-06 11:24:39 -0700

    edits made to code

--- randomizer.3/sketch.js
+++ randomizer.3/sketch.js
@@ -1,25 +1,25 @@
 // Parks that I have visted with my family <3
 let parks = [{
   name: "yellowstone",
-  color: "yellow"
+  color: "yellowy"
 }, {
   name: "grand canyon",
-  color: "rust"
+  color: "rusty"
 }, {
   name: "sequioa",
-  color: "forest"
+  color: "foresty"
 }, {
-  name: "death",
+  name: "DUNE",
   color: "sandy"
 }, {
   name: "bryce",
-  color: "orange"
+  color: "orangey"
 }, {
   name: "arches",
-  color: "mars"
+  color: "marsy"
 }, {
   name: "zion",
-  color: "valleygreen"
+  color: "valleygreeny"
 }, {
   name: "Yosemite",
   color: "snowy"
@@ -28,13 +28,15 @@ let parks = [{
 let randomIndex;
 let animating = false;
 let dinosaurs = [];
+let imageCounter = 0;
+let button;
 
 //image function "preload", declare a variable for the images//
 function preload() {
 
   //create an array for the list of images//
-  for (let i = 0; i <= 5; i++){
-    dinosaurs[i] = loadImage(`assets/dinosaur_${i}.png`)
+  for (let i = 0; i <= 5; i++) {
+    dinosaurs[i] = loadImage("assets/dinosaur_" + i + ".png")
     //"i is the variable number" ALT way of writing the code (`assets/troll_${i}.JPG`)//
   }
 
@@ -42,44 +44,58 @@ function preload() {
 
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(700, 700);
   background(150);
   textSize(35, color(random(255)));
+  textAlign(CENTER);
+  imageMode(CENTER);
+  frameRate(5);
 
-  text("click to randomize", 50, 50);
-  console.log(dinosaur);
+  text("click to randomize", 50, 50, width / 2, height / 2);
+  console.log(dinosaurs);
 
+  button = createButton("click to randomize!");
+  button.mousePressed(buttonPressed);
+  button.class("randomizerButton");
 
 }
 
 function draw() {
 
   if (animating == true) {
-    stroke(random(width), random(height), random(25, 200))
-    fill(random(255))
-    ellipse(random(width), random(height), random(255))
+    clear();
+    image(dinosaurs[imageCounter], width / 2, height / 2);
 
-  }
+    if (imageCounter < dinosaurs.length) {
+      imageCounter++;
+      console.log(imageCounter);
+    } else {
+      imageCounter = 0;
+      }
+    }
+
 }
 
 function randomizer() {
   animation = false;
   if (parks[0]) {
     //displays random name and splices it out of the array list
-    background(random(80, 150));
+    //background(random(80, 150));
+    clear();
     randomIndex = int(random(parks.length));
-    text(`${parks[randomIndex].name}'s favorite color is
-    ${parks[randomIndex].color}`, 100, 200);
+    text(` A ${parks[randomIndex].color} dinosaur named ${parks[randomIndex].name}
+    pops out of the egg!`,width / 2, height - 10);
+    image(random(dinosaurs), width / 2, height / 2);
     //text(parks[randomIndex].name + "'s favorite color is" +
     //parks[randomIndex].color, 50, 50);
     parks.splice(randomIndex, 1);
   } else {
     background(random(80, 150));
-    text("nothing left!", 100, 200);
+    text("nothing left!", 100, 200, width / 2, height / 2);
   }
 }
 
-function mousePressed() {
+function buttonPressed() {
   animating = true;
   setTimeout(randomizer, 2000);
 }
